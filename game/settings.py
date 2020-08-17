b4920ecaec6f16f6bc37f50f64312b3d4f4ce30c
Ivo Fokkema <Ivo@IFokkema.nl>
2020-08-17 20:05:57 +0200

    Added deGetUnitLocations() that builds an array holding the given unit's positions. - 6, 3, 2, or 1 unit can be passed. deGetUnitLocations() sorts the units on strength and calculates the correct positions for each unit.

--- game/settings.py
+++ game/settings.py
@@ -38,6 +38,62 @@ def deCreateNewPlayer(name):
     return True
 
 
+def deGetUnitLocations(aUnits):
+    # Based on the unit types and their strengths, calculate which unit goes where.
+    aXP = []
+    aPositions = []  # Positions of units, in the order in which the units were given.
+    nXOffset = 350   # Where to start putting units.
+    nYDistance = 40  # Y distance between units.
+
+    if (len(aUnits) not in (6, 3, 2, 1)):
+        print("Player's savegame is currupt; unit count is not 6, 3, 2, or 1.")
+        exit(1)
+
+    # Python's lists/dicts/tuples are very frustrating if you come from PHP...
+    # I would just collect the unit's keys and their values, then sort on the values and keep the keys, but no...
+    # Even looping over a simple list, keeping index, is super complicated, and requires the list to be converted.
+    for (nKey, aUnit) in enumerate(aUnits):
+        aXP.append((nKey, ((aUnit['xp'] * 100) + aUnit['xp_shots'])) * (1 if (aUnit['body'][0] == 'humans') else (10 if (aUnit['body'][0] == 'tanks') else 100)))
+
+    # "sort" the XP list, so I know in which order I need to place these units.
+    aXP = sorted(aXP, key=lambda x: x[1], reverse=True)
+    # [(index, XP), (index, XP), (index, XP), ...]
+
+    if len(aUnits) == 6:
+        aYs = [-0.5, 0.5, -1.5, 1.5, -2.5, 2.5]
+        i = 0
+        for (nKey, nXP) in aXP:
+            # nKey here is the key in the aUnits, not the current key (i).
+            if i == 2 or i == 4:
+                nXOffset -= (0.5 * nYDistance)
+            aPositions.append((nKey, nXOffset, (WINDOW_HEIGHT/2) + (aYs[i]) * nYDistance))
+            i += 1
+
+    elif len(aUnits) == 3:
+        aYs = [0, -2.5, 2.5]
+        i = 0
+        for (nKey, nXP) in aXP:
+            # nKey here is the key in the aUnits, not the current key (i).
+            if i == 1:
+                nXOffset -= nYDistance
+            aPositions.append((nKey, nXOffset, (WINDOW_HEIGHT/2) + (aYs[i]) * nYDistance))
+            i += 1
+
+    elif len(aUnits) == 2:
+        i = 0
+        for (nKey, nXP) in aXP:
+            # nKey here is the key in the aUnits, not the current key (i).
+            if i == 1:
+                nXOffset /= 2
+            aPositions.append((nKey, nXOffset, (WINDOW_HEIGHT/2)))
+            i += 1
+
+    else:
+        aPositions.append((0, nXOffset, (WINDOW_HEIGHT/2)))
+
+    return aPositions
+
+
 def deInitSaveGames():
     global SAVEGAMES, SAVEGAMES_FILE
     # Load our savegames.
